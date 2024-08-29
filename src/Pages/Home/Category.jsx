269ee125696e3b9223e1991bef13e8ab9cbdadb9
Mdafsarx <mdafsar99009@gmail.com>
2024-08-29 19:59:13 +0600

    category section done alhamduliah

--- src/Pages/Home/Category.jsx
+++ src/Pages/Home/Category.jsx
@@ -4,19 +4,31 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import CategoryCard from './CategoryCard';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Category() {
+
+    const [Data, setData] = useState([]);
+
+    useEffect(() => {
+        axios(`http://localhost:3000/Product`)
+            .then(data => setData(data.data))
+            .catch(error => console.log(error))
+    }, [])
+    const computers = Data.filter(data => data.category === "Computers")
+
     return (
         <section className="text-white pb-14 pt-6 max-w-6xl mx-auto">
 
             {/* computers */}
-            <div className="flex gap-4">
+            <div className="flex gap-5 pb-10 border-b border-[#3CA2FA]">
 
                 {/* category name and title */}
                 <div className='w-[25%]'>
-                    <h1>Laptop</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    <button className="uppercase font-normal pt-0.5 border-b-2 border-white hover:text-[#3CA2FA] hover:border-[#3CA2FA]">Shop Now</button>
+                    <h1 className='font-black font-mono text-3xl uppercase text-[#80EEB4] pb-1'>Computers</h1>
+                    <p>All the power I need in one sleek device.</p>
+                    <button className="uppercase font-normal pt-3 border-b-2 border-white hover:text-[#3CA2FA] hover:border-[#3CA2FA]">Shop Now</button>
                 </div>
                 {/* products */}
                 <div className='w-[75%]'>
@@ -29,11 +41,10 @@ export default function Category() {
                         modules={[Pagination]}
                         className="mySwiper">
 
-                        <SwiperSlide><CategoryCard /></SwiperSlide>
-                        <SwiperSlide><CategoryCard /></SwiperSlide>
-                        <SwiperSlide><CategoryCard /></SwiperSlide>
-                        <SwiperSlide><CategoryCard /></SwiperSlide>
-
+                        {
+                           computers?.map((data,i)=><SwiperSlide key={i}><CategoryCard product={data}/></SwiperSlide>)
+                        }
+                        
                     </Swiper>
                 </div>
 
--- src/Pages/Home/CategoryCard.jsx
+++ src/Pages/Home/CategoryCard.jsx
@@ -1,15 +1,20 @@
-export default function CategoryCard() {
+export default function CategoryCard({product}) {
+    const { productName, description, price, category, ratings, creationDate, image, brand } = product || {};
+
     return (
         <div>
             <div className="w-full max-w-xs overflow-hidden border-2 rounded pb-2.5">
                 <img
                     className="object-cover w-full h-56"
-                    src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                    alt="avatar" />
+                    src={image}
+                    alt={productName} />
 
                 <div className="p-4 text-left">
-                    <h2 className="block text-xl font-bold"> John Doe </h2>
-                    <span className="text-sm">Software Engineer</span>
+                    <h2 className="block text-xl font-bold">{productName}</h2>
+                  <div className="flex items-center justify-between pt-1">
+                      <p className="text-sm">${price}</p>
+                      <button className="btn btn-sm text-xs bg-[#80EEB4] hover:bg-[#3CA2FA] hover:text-white border-0 text-black">Add to cart</button>
+                  </div>
                 </div>
             </div>
         </div>
