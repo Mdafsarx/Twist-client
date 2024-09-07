d96160d21d482b4da3cc5a8ac8435443b533eda3
Mdafsarx <mdafsar99009@gmail.com>
2024-09-07 21:52:25 +0600

    add

--- src/Pages/Home/AllItems.jsx
+++ src/Pages/Home/AllItems.jsx
@@ -9,12 +9,12 @@ export default function AllItems() {
         <section className="text-white pb-14 max-w-6xl mx-auto">
 
             <h1 className="text-3xl font-medium font-mono text-[#3CA2FA] text-center md:text-start"><span className="border-b-2 border-b-[#3CA2FA] pb-1">Popul</span>ar</h1>
-
+    
             <div className="flex flex-col md:flex-row items-center pt-6">
                 <img src={img} className="w-[231px] rounded-t-2xl md:rounded-l-xl h-52"  data-aos="zoom-in"/>
-                <img src={img4} className="w-[231px] h-52" data-aos="zoom-in" data-aos-duration="700" data-aos-delay="200"/>
-                <img src={img5} className="w-[231px] h-52" data-aos="zoom-in" data-aos-duration="700" data-aos-delay="300"/>
-                <img src={img2} className="w-[231px] h-52" data-aos="zoom-in" data-aos-duration="700" data-aos-delay="400"/>
+                <img src={img4} className="w-[231px] h-52" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="200"/>
+                <img src={img5} className="w-[231px] h-52" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="300"/>
+                <img src={img2} className="w-[231px] h-52" data-aos="zoom-in" data-aos-duration="500" data-aos-delay="400"/>
                 <img src={img3} className="w-[231px] rounded-b-2xl md:rounded-r-xl h-52" data-aos="zoom-in" data-aos-duration="700" data-aos-delay="500"/>
             </div>
 
